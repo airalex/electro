576f4dfb73a8714e6f84469e4805e185fad4027e
Alexander Juda <alexander.juda@icloud.com>
2016-11-11 17:32:16 +0100

    Create Maven project from quickstart archetype, set to Java 8

---
+++ src/main/java/com/alexanderjuda/electro/App.java
@@ -0,0 +1,13 @@
+package com.alexanderjuda.electro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
